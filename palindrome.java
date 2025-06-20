ad10293c710e2667dc4b891aee6c0e97b92d5732
Dhanushree9095 <155515492+Dhanushree9095@users.noreply.github.com>
2025-06-20 09:40:39 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false; // Not a palindrome
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+        String word = "madam";
+
+        if (isPalindrome(word)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
+    }
+}
